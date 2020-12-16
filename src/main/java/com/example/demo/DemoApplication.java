6f4270f5c008cc8f0e6ba2a89686beb3f59a0b0b
刘钦 <liuqin@liuqindeMacBook-Pro.local>
2020-12-16 08:58:24 +0800

    对接python爬虫和后端

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,12 +3,27 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(DemoApplication.class, args);
-        System.out.println("hello world");
+        try {
+            Process process = Runtime.getRuntime().exec("python3 /Users/liuqin/PycharmProjects/stockBot/speculator1.py");
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                System.out.println(line);
+            }
+            in.close();
+            int re = process.waitFor();
+            System.out.println(re);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
-
 }
