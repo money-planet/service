0422ca78f3e66c7adbc5bec3aa8fbf82a8298cec
刘钦 <liuqin@liuqindeMacBook-Pro.local>
2020-12-17 08:54:37 +0800

    添加一个controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,7 +10,7 @@ import java.io.InputStreamReader;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         try {
             Process process = Runtime.getRuntime().exec("python3 /Users/liuqin/PycharmProjects/stockBot/speculator1.py");
